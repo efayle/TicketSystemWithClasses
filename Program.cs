5b351e3b7bb2de57cf58daa27eef292a9358ac28
Elena Fayle <efayle@my.wctc.edu>
2021-03-05 14:58:19 -0600

    Display all tickets

--- Program.cs
+++ Program.cs
@@ -13,12 +13,18 @@ namespace TicketSystemWithClasses
             logger.Info("Program started");
 
             TicketFile ticketFile = new TicketFile(ticketFilePath);
-            
+
             string choice;
 
             do {
                 Console.WriteLine("1.) Display tickets\n2.) Create tickets\n3.) Exit");
                 choice = Console.ReadLine();
+                if (choice == "1") {
+                    foreach(Ticket t in ticketFile.Tickets)
+                    {
+                        Console.WriteLine(t.Display());
+                    }
+                }
 
             } while (choice == "1" || choice == "2");
 
