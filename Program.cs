ef275a672e8411f7d6221a68804414ccccb2c523
Elena Fayle <efayle@my.wctc.edu>
2021-03-05 14:43:21 -0600

    Menu options

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
 
 namespace TicketSystemWithClasses
 {
     class Program
     {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            logger.Info("Program started");
+
+            string choice;
+
+            do {
+                Console.WriteLine("1.) Display tickets\n2.) Create tickets\n3.) Exit");
+                choice = Console.ReadLine();
+
+            } while (choice == "1" || choice == "2");
+
+            logger.Info("Program ended");
         }
     }
 }
