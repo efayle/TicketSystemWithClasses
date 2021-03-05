98d5976a495b4918fbbb90404ed28eb8426a5c6c
Elena Fayle <efayle@my.wctc.edu>
2021-03-05 14:57:04 -0600

    Populate list, read from file

--- TicketFile.cs
+++ TicketFile.cs
@@ -9,10 +9,39 @@ namespace TicketSystemWithClasses
     public class TicketFile
     {
         public string filePath { get; set; }
+        public List<Ticket> Tickets { get; set;}
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
         public TicketFile (string ticketFilePath)
         {
             filePath = ticketFilePath;
+            Tickets = new List<Ticket>();
+
+            try 
+            {
+                StreamReader sr = new StreamReader(filePath);
+                    while (!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+                        string[] array = line.Split(',');
+                        // create ticket from line of data
+                        Ticket ticket = new Ticket();
+                        ticket.ticketID = Convert.ToInt32(array[0]);
+                        ticket.ticketSummary = array[1];
+                        ticket.ticketStatus = array[2];
+                        ticket.ticketPriority = array[3];
+                        ticket.ticketSubmitter = array[4];
+                        ticket.ticketAssigner = array[5];
+                        ticket.ticketWatchers = array[6].Split('|').ToList();
+                        // save ticket to List
+                        Tickets.Add(ticket);
+                    }
+                    sr.Close();  
+            }
+            catch (Exception ex) 
+            {
+                logger.Error(ex.Message);
+            }
+        
         }
     }
 }
